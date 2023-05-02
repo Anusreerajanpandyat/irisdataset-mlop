67374347a8351c265f6ecb368673170cc2597901
Anusreerajanpandyat <130542745+Anusreerajanpandyat@users.noreply.github.com>
2023-05-02 11:46:28 +0530

    Update save_model.py

--- documentation/src/models/model1/save_model.py
+++ documentation/src/models/model1/save_model.py
@@ -2,7 +2,7 @@ import os
 import pickle
 from train import clf, model_path
 
-model_path = 'C:/Users/243415/Documents/irisdataset/documentation/models/svm_model.pkl'
+model_path = 'models/svm_model.pkl'
 
 # Save the trained model
 os.makedirs(os.path.dirname(model_path), exist_ok=True)
